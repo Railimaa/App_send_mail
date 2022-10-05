dc79a3ef57075f86fa3ea2125da0cddc9eaa3445
Rai Lima <ferreiralimarai0@gmail.com>
2022-10-05 19:37:57 -0300

    App email

---
+++ processa_envio.php
@@ -0,0 +1,10 @@
+<?php 
+
+
+require "../../app_send_mail/PHPMailer/Exception.php";
+require "../../app_send_mail/PHPMailer/PHPMailer.php";
+require "../../app_send_mail/PHPMailer/SMTP.php";
+
+
+
+require   "../../app_send_mail/processa_envio.php"; 
